2101494d5258df45e3810b4dcb7b2e2feaed52be
secondskoll <michael.park@canonical.com>
2026-01-05 14:50:37 +1000

    test logging

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -1,5 +1,16 @@
 // overwrite links.js
 
+console.stdlog = console.log.bind(console);
+console.logs = [];
+
+console.log = function() {
+    // 1. Store the arguments in the logs array
+    console.logs.push(Array.from(arguments));
+
+    // 2. Call the original console.log to still output to the browser console
+    console.stdlog.apply(console, arguments);
+};
+
 // Replace oldDomain with newDomain
 const oldDomain = 'canonical-rtd-testing.readthedocs-hosted.com';
 const newDomain = 'staging.canonical.com/product_1';
@@ -8,10 +19,13 @@ const newDomain = 'staging.canonical.com/product_1';
 const shadowHost = document.querySelector('readthedocs-flyout');
 const shadowRoot = shadowHost.shadowRoot;
 
+console.log("logging...")
+
 if (shadowRoot) {
     var anchors = shadowRoot.querySelectorAll('a');
     for (var i = 0; i < anchors.length; i++) {
         // Use a regular expression with the 'g' flag for global replacement
         anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
+        console.log(`Replaced URL in shadow DOM: ${anchors[i].href}`);
     }
 }
