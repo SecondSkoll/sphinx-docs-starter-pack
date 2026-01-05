c2060d6b89c60f0e82d01cc6b676a06d7af45024
secondskoll <michael.park@canonical.com>
2026-01-05 14:19:35 +1000

    test: overwrite shadow root

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -1,13 +1,17 @@
-// overwrite_links_specific.js
-document.addEventListener('DOMContentLoaded', function() {
-    window.onload = function() {
-        var anchors = document.getElementsByTagName("a");
-        var oldDomain = "canonical-rtd-testing.readthedocs-hosted.com";
-        var newDomain = "staging.canonical.com";
+// overwrite links.js
 
-        for (var i = 0; i < anchors.length; i++) {
-            // Use a regular expression with the 'g' flag for global replacement
-            anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
-        }
-    };
-});
+// Replace oldDomain with newDomain
+const oldDomain = 'canonical-rtd-testing.readthedocs-hosted.com';
+const newDomain = 'staging.canonical.com/product_1';
+
+// Access the shadow DOM of the 'readthedocs-flyout' element
+const shadowHost = document.querySelector('readthedocs-flyout');
+const shadowRoot = shadowHost.shadowRoot;
+
+if (shadowRoot) {
+    var anchors = shadowRoot.querySelectorAll('a');
+    for (var i = 0; i < anchors.length; i++) {
+        // Use a regular expression with the 'g' flag for global replacement
+        anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
+    }
+}
