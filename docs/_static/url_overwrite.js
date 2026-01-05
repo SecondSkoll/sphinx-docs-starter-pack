ce652cff9312aa110324fea9ec7affa86d6d5248
secondskoll <michael.park@canonical.com>
2026-01-05 17:01:59 +1000

    anothertest

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -21,10 +21,26 @@ const shadowRoot = shadowHost.shadowRoot;
 
 console.log("logging...")
 
-var anchors = shadowRoot.querySelectorAll('a');
-for (var i = 0; i < anchors.length; i++) {
-    // Use a regular expression with the 'g' flag for global replacement
-    console.log(`Checking URL for replacement: ${anchors[i].href}`);
-    anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
-    console.log(`URL now: ${anchors[i].href}`);
+// if (shadowRoot) {
+//     const anchors = shadowRoot.querySelectorAll('a');
+//     for (let i = 0; i < anchors.length; i++) {
+//         // Use a regular expression with the 'g' flag for global replacement
+//         console.log(`Checking URL for replacement: ${anchors[i].href}`);
+//         anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
+//         console.log(`URL now: ${anchors[i].href}`);
+//     }
+// }
+
+window.addEventListener('load', function() {
+    if (shadowRoot) {
+        const anchors = shadowRoot.querySelectorAll('a');
+        anchors.forEach(anchor => {
+            // Use a regular expression with the 'g' flag for global replacement
+            console.log(`Checking URL for replacement: ${anchor.href}`);
+            anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
+            console.log(`URL now: ${anchor.href}`);
+        }
+        );
+    }
 }
+);
