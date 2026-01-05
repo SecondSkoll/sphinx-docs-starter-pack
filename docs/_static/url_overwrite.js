2bf297f0812c6d8bb208292dacaf0e94da3e75ef
secondskoll <michael.park@canonical.com>
2026-01-05 13:28:23 +1000

    test: add listener

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -1,11 +1,13 @@
 // overwrite_links_specific.js
-window.onload = function() {
-    var anchors = document.getElementsByTagName("a");
-    var oldDomain = "canonical-rtd-testing.readthedocs-hosted.com";
-    var newDomain = "staging.canonical.com";
+document.addEventListener('DOMContentLoaded', function() {
+    window.onload = function() {
+        var anchors = document.getElementsByTagName("a");
+        var oldDomain = "canonical-rtd-testing.readthedocs-hosted.com";
+        var newDomain = "staging.canonical.com";
 
-    for (var i = 0; i < anchors.length; i++) {
-        // Use a regular expression with the 'g' flag for global replacement
-        anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
-    }
-};
+        for (var i = 0; i < anchors.length; i++) {
+            // Use a regular expression with the 'g' flag for global replacement
+            anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
+        }
+    };
+});
