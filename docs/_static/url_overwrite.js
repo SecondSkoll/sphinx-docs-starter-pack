efefbc4675aac47336f46e0819a9f98a2ae04884
secondskoll <michael.park@canonical.com>
2026-01-05 17:12:27 +1000

    fix: FINAL

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -1,5 +1,6 @@
 // overwrite links.js
 
+// Logging for debugging purposes
 console.stdlog = console.log.bind(console);
 console.logs = [];
 
@@ -19,25 +20,13 @@ const newDomain = 'staging.canonical.com/product_1';
 const shadowHost = document.querySelector('readthedocs-flyout');
 const shadowRoot = shadowHost.shadowRoot;
 
-console.log("logging...")
-
-// if (shadowRoot) {
-//     const anchors = shadowRoot.querySelectorAll('a');
-//     for (let i = 0; i < anchors.length; i++) {
-//         // Use a regular expression with the 'g' flag for global replacement
-//         console.log(`Checking URL for replacement: ${anchors[i].href}`);
-//         anchors[i].href = anchors[i].href.replace(new RegExp(oldDomain, 'g'), newDomain);
-//         console.log(`URL now: ${anchors[i].href}`);
-//     }
-// }
-
+// On window load, find all relevant anchor tags and replace URLs
 window.addEventListener('load', function() {
         const anchors = shadowRoot.querySelectorAll('a');
         anchors.forEach(anchor => {
-            // Use a regular expression with the 'g' flag for global replacement
-            console.log(`Checking URL for replacement: ${anchor.href}`);
+            // console.log(`Checking URL for replacement: ${anchor.href}`);
             anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
-            console.log(`URL now: ${anchor.href}`);
+            // console.log(`URL now: ${anchor.href}`);
         }
         );
     }
