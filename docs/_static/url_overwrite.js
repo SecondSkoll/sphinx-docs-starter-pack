1359ae28fdc6dc770b48d07100d0649856814c83
secondskoll <michael.park@canonical.com>
2026-01-06 15:44:46 +1000

    add logging

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -31,6 +31,7 @@ function waitForElement(element, callback){
 }
 
 waitForElement("readthedocs-flyout", function(){
+    console.log(`Triggering URL rewrite`)
     const rtdFlyout = document.querySelector('readthedocs-flyout');
     rtdFlyout.addEventListener('load', function() {
         setTimeout(() => {
