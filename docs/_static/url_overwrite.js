a57446a009c5679b08882fbd102b21f33ae09697
secondskoll <michael.park@canonical.com>
2026-01-06 15:13:27 +1000

    Refactor w/ wait for element

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -4,49 +4,47 @@
 const oldDomain = 'canonical-rtd-testing.readthedocs-hosted.com';
 const newDomain = 'staging.canonical.com/product_1/docs';
 
-// // Logging for debugging purposes - DISABLED
-// console.stdlog = console.log.bind(console);
-// console.logs = [];
+// Logging for debugging purposes
+console.stdlog = console.log.bind(console);
+console.logs = [];
 
-// console.log = function() {
-//     // 1. Store the arguments in the logs array
-//     console.logs.push(Array.from(arguments));
+console.log = function() {
+    // 1. Store the arguments in the logs array
+    console.logs.push(Array.from(arguments));
 
-//     // 2. Call the original console.log to still output to the browser console
-//     console.stdlog.apply(console, arguments);
-// };
+    // 2. Call the original console.log to still output to the browser console
+    console.stdlog.apply(console, arguments);
+};
 
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
 
-const callback = function(mutationsList, observer) {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            const rtdFlyout = document.querySelector('readthedocs-flyout');
-            if (rtdFlyout) {
-
-                rtdFlyout.addEventListener('load', function() {
-                    setTimeout(() => {
-                    rtdFlyout.addEventListener('click', function(e) {
-                        // Access the shadow DOM of the 'readthedocs-flyout' element
-                        const shadowRoot = rtdFlyout.shadowRoot;
-                        const anchors = shadowRoot.querySelectorAll('a');
-                        anchors.forEach(anchor => {
-                            console.log(`Checking URL for replacement: ${anchor.href}`);
-                            anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
-                            console.log(`URL now: ${anchor.href}`);
-                        }
-                        )
-                        },
-                    observer.disconnect()
-                    );}, 1000);
-                }
-                )
-            }
-
+function waitForElement(element, callback){
+    var foo = setInterval(function(){
+        console.log(`Waiting for element: ${element}...`)
+        if(document.querySelector(element)){
+            clearInterval(foo);
+            callback();
+        console.log(`Element found: ${element}.`)
         }
-    }
-};
+    }, 100);
+}
 
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+waitForElement("readthedocs-flyout", function(){
+    rtdFlyout.addEventListener('load', function() {
+        setTimeout(() => {
+        rtdFlyout.addEventListener('click', function(e) {
+            // Access the shadow DOM of the 'readthedocs-flyout' element
+            const shadowRoot = rtdFlyout.shadowRoot;
+            const anchors = shadowRoot.querySelectorAll('a');
+            anchors.forEach(anchor => {
+                console.log(`Checking URL for replacement: ${anchor.href}`);
+                anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
+                console.log(`URL now: ${anchor.href}`);
+            }
+            )
+            }
+        );}, 1000);
+    }
+    );
+});
