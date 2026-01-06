cfbe0f556caf6a1a191103fc9aee66f02b4a9f06
secondskoll <michael.park@canonical.com>
2026-01-06 16:05:42 +1000

    Production flyout rewrite

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -4,43 +4,43 @@
 const oldDomain = 'canonical-rtd-testing.readthedocs-hosted.com';
 const newDomain = 'staging.canonical.com/product_1/docs';
 
-// Logging for debugging purposes
-console.stdlog = console.log.bind(console);
-console.logs = [];
+// // Logging for debugging purposes
+// console.stdlog = console.log.bind(console);
+// console.logs = [];
 
-console.log = function() {
-    // 1. Store the arguments in the logs array
-    console.logs.push(Array.from(arguments));
+// console.log = function() {
+//     // 1. Store the arguments in the logs array
+//     console.logs.push(Array.from(arguments));
 
-    // 2. Call the original console.log to still output to the browser console
-    console.stdlog.apply(console, arguments);
-};
+//     // 2. Call the original console.log to still output to the browser console
+//     console.stdlog.apply(console, arguments);
+// };
 
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
 
 function waitForElement(element, callback){
     var foo = setInterval(function(){
-        console.log(`Waiting for element: ${element}...`)
+        // console.log(`Waiting for element: ${element}...`)
         if(document.querySelector(element)){
             clearInterval(foo);
+            // console.log(`Element found: ${element}.`)
             callback();
-        console.log(`Element found: ${element}.`)
         }
     }, 100);
 }
 
 waitForElement("readthedocs-flyout", function(){
-    console.log(`Triggering URL rewrite`)
+    // console.log(`Triggering URL rewrite`)
     const rtdFlyout = document.querySelector('readthedocs-flyout');
     rtdFlyout.addEventListener('click', function(e) {
         // Access the shadow DOM of the 'readthedocs-flyout' element
         const shadowRoot = rtdFlyout.shadowRoot;
         const anchors = shadowRoot.querySelectorAll('a');
         anchors.forEach(anchor => {
-            console.log(`Checking URL for replacement: ${anchor.href}`);
+            // console.log(`Checking URL for replacement: ${anchor.href}`);
             anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
-            console.log(`URL now: ${anchor.href}`);
+            // console.log(`URL now: ${anchor.href}`);
         });
     });
 });
