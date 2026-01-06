261b9c173a4a74cf7cd6b4497cb7a40d3b27bc12
secondskoll <michael.park@canonical.com>
2026-01-06 15:57:20 +1000

    test

--- docs/_static/url_overwrite.js
+++ docs/_static/url_overwrite.js
@@ -33,20 +33,14 @@ function waitForElement(element, callback){
 waitForElement("readthedocs-flyout", function(){
     console.log(`Triggering URL rewrite`)
     const rtdFlyout = document.querySelector('readthedocs-flyout');
-    rtdFlyout.addEventListener('load', function() {
-        setTimeout(() => {
-        rtdFlyout.addEventListener('click', function(e) {
-            // Access the shadow DOM of the 'readthedocs-flyout' element
-            const shadowRoot = rtdFlyout.shadowRoot;
-            const anchors = shadowRoot.querySelectorAll('a');
-            anchors.forEach(anchor => {
-                console.log(`Checking URL for replacement: ${anchor.href}`);
-                anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
-                console.log(`URL now: ${anchor.href}`);
-            }
-            )
-            }
-        );}, 1000);
-    }
-    );
+    rtdFlyout.addEventListener('click', function(e) {
+        // Access the shadow DOM of the 'readthedocs-flyout' element
+        const shadowRoot = rtdFlyout.shadowRoot;
+        const anchors = shadowRoot.querySelectorAll('a');
+        anchors.forEach(anchor => {
+            console.log(`Checking URL for replacement: ${anchor.href}`);
+            anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
+            console.log(`URL now: ${anchor.href}`);
+        });
+    });
 });
